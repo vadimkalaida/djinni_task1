aa92ad23048b92700f1042841e1ec012801b82b9
vadimkalayda <vadosgta1@gmail.com>
2021-01-18 18:02:41 +0200

    earnings and status is ready

--- task1/task1/src/components/MainComponent/MainComponent.js
+++ task1/task1/src/components/MainComponent/MainComponent.js
@@ -5,9 +5,11 @@ import { Main, MainContainer, MainLeftPart, MainNotificationsBlock, MainTitle, M
 const MainComponent = ( ) => {
   const numberOfScores = Math.floor(Math.random() * 100) * 10;
   const numberOfViews = Math.floor(Math.random() * 10000);
+  const [ totalEarnings, setTotalEarnings ] = useState();
   const [ earningsForDays, setEarningsForDays ] = useState(0);
 
-  const getEarningsForDay = () => {
+  const getEarningsForMonth = () => {
+    // get earnings for month
     let maxNumberOfMoneyForDay = 283.24;
     let amountOfDays = new Date().getDate();
     let sumOfMoney = 0;
@@ -18,7 +20,18 @@ const MainComponent = ( ) => {
     setEarningsForDays(sumOfMoney.toFixed(2));
   };
 
-  useEffect(getEarningsForDay, []);
+  useEffect(getEarningsForMonth, []);
+
+  const getFullAmountOfEarnings = () => {
+    let actionWithEarnings = Math.floor(Math.random() * 2);
+    if(actionWithEarnings === 0) {
+      setTotalEarnings(earningsForDays / 2);
+    } else {
+      setTotalEarnings(earningsForDays * 2);
+    }
+  };
+
+  useEffect(getFullAmountOfEarnings, [earningsForDays]);
 
   return(
     <Main>
@@ -40,7 +53,7 @@ const MainComponent = ( ) => {
           <MainRightPartBlock>
             <MainRightPartTitle>Earnings</MainRightPartTitle>
             <MainRightPartParagraph>Total</MainRightPartParagraph>
-            <MainRightPartEarningsNumber>4291.65&euro;</MainRightPartEarningsNumber>
+            <MainRightPartEarningsNumber>{ totalEarnings }&euro;</MainRightPartEarningsNumber>
             <MainRightPartLine></MainRightPartLine>
             <MainRightPartParagraph>Your earnings on { new Date().toLocaleString('default', { month: 'long' }) }</MainRightPartParagraph>
             <MainRightPartEarningsNumber>{ earningsForDays }&euro;</MainRightPartEarningsNumber>
